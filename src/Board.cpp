2a035cb9ea9d89443c7876b68074f2308e21b9db
s3622250 <s3622250@student.rmit.edu.au>
2021-04-30 11:53:01 +1000

    Update Board

--- src/Board.cpp
+++ src/Board.cpp
@@ -12,22 +12,7 @@ void Board::addTile(Tile* tile)
 
 void Board::printBoard()
 {
-    for(uint32_t i = 0; i < BOARD_SIZE; i++)
-    {
-        for(uint32_t j = 0; j < BOARD_SIZE; j++)
-        {
-            for(int k = 0; k < this->board.size(); k++)
-            {
-                Tile* curTile = this->board.at(k);
-                Row row = curTile->row;
-                Col col = curTile->col;
-                if(getRow(row) == i && col == j)
-                {
-                    std::cout << curTile->shape << curTile->colour << std::endl;
-                }
-            }
-        }
-    }
+    
 }
 
 int Board::getRow(Row row)
