32d60c370675697ec91084b8c1ba8a4953911359
Your Name <s3545259@student.rmit.edu.au>
2021-05-04 15:04:50 +1000

    Initial Linkedlist code

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -3,9 +3,40 @@
 
 LinkedList::LinkedList() {
    head = nullptr;
-
-   // TODO
+   tail = nullptr;
+   
 }
 
 LinkedList::~LinkedList() {
+}
+
+void LinkedList::removeFront() {
+  if (head != nullptr) {
+    Node* temp = head;
+    head = temp->next;
+    delete temp;
+  }
+}
+
+void LinkedList::removeBack() {
+  Node* temp = head;
+  Node* prev = nullptr;
+  while (temp->next != nullptr) {
+    prev = temp;
+    temp = temp->next;
+  }
+  prev->next = nullptr;
+  tail = prev;
+  delete temp;
+}
+
+void LinkedList::addTile(Tile* tile) {
+  Node* node = new Node(tile, nullptr);
+  if (tail != nullptr) {
+    tail->next = node;
+    tail = node;
+  } else {
+    head = node;
+    tail = node;
+  }
 }
