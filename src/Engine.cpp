aed2372114621684dabc3d04b33bab3f91d6b7ff
s3622250 <s3622250@student.rmit.edu.au>
2021-05-06 14:53:00 +1000

    Fixed regex_search for save

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -68,7 +68,7 @@ void Engine::gameRun()
             if(std::regex_match(option, std::regex("^(save) ([a-z0-9]+)$")))
             {
                 std::smatch match;
-                if(std::regex_search(option, match, std::regex("^(place) ([R|O|Y|G|B|P][1-6]) (at) ([A-Z][0-25])$")))
+                if(std::regex_search(option, match, std::regex("^(save) ([a-z0-9]+)$")))
                 {
                     string filename = match.str(REGEX_SAVE);
                     saveGame(filename);
