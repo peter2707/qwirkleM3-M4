9030d32e54d81912ab0626d5ea671dc9dbeab21d
soklenglim <69133654+soklenglim@users.noreply.github.com>
2021-05-08 12:38:31 +1000

    Revert "working on giveTile and set player hand"

--- src/Node.h
+++ src/Node.h
@@ -3,16 +3,15 @@
 #define ASSIGN2_NODE_H
 
 #include "Tile.h"
-#include <iostream>
 
 class Node {
 public:
 
-   Node(std::shared_ptr<Tile> tile, std::shared_ptr<Node>  next);
-   //Node(Node& other);
+   Node(Tile* tile, Node* next);
+   Node(Node& other);
 
-   std::shared_ptr<Tile>   tile;
-   std::shared_ptr<Node>   next;
+   Tile*    tile;
+   Node*    next;
 };
 
 #endif // ASSIGN2_NODE_H
